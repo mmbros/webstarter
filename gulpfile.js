7d22c6965b9e7fa93aaae3488114ee204a33ef62
mmbros <server.mmbros@yandex.com>
2018-01-04 22:35:45 +0100

    added normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,61 @@
+'use strict';
+
 const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
+const rename      = require('gulp-rename');
+
+var
+  src = 'src/',
+  dest = 'dist/',
+  tmp = 'tmp/';
+
+
+var scss = {
+  src: src + 'scss/style.scss',
+  dest: dest + 'css/',
+  sassOpts: {
+    includePaths: [tmp],
+    outputStyle: 'compressed'
+  }
+}
+
+gulp.task('normalize', function() {
+  return gulp.src('./node_modules/normalize.css/normalize.css')
+    .pipe(rename('_normalize.scss'))
+    .pipe(gulp.dest(tmp));
+});
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-	return gulp.src(['src/scss/*.scss'])
-		.pipe(sass())
-		.pipe(gulp.dest("build/css"))
-		.pipe(browserSync.stream());
+gulp.task('sass', ['normalize'], function() {
+  return gulp.src(scss.src)
+    .pipe(sass(scss.sassOpts))
+    .pipe(gulp.dest(scss.dest))
+    .pipe(browserSync.stream());
 });
 
+// copy:html
+gulp.task('copy:html', function() {
 
-//
-gulp.task('html', function() {
-	return gulp.src(['src/*.html'])
-		.pipe(gulp.dest("build/"))
-		.pipe(browserSync.stream());
+  return gulp.src(['src/**/*.html'])
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.stream());
 });
+
+// copy
+gulp.task('copy', [
+  'copy:html'
+]);
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass','html'], function() {
+gulp.task('serve', ['copy', 'sass'], function() {
 
     browserSync.init({
-        server: "./build"
+        server: "./dist"
     });
 
     gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch(['src/*.html'], ['html']);
+    gulp.watch(['src/**/*.html'], ['copy:html']);
 });
 
 
